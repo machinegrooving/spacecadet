f25f228aaaf6e9cb43984fc206069290750a4ec3
dmrib <dmrib.cs@gmail.com>
2019-10-15 19:41:34 -0300

    Create p5.js project

---
+++ src/sketch.js
@@ -0,0 +1,21 @@
+/**
+ * p5.js setup.
+ * 
+ * Returns:
+ *  undefined.
+ */
+function setup() 
+{
+
+}
+
+/**
+ * p5.js drawing callback.
+ * 
+ * Returns:
+ *  undefined.
+ */
+function draw() 
+{
+
+}
