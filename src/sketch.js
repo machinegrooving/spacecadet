16e6abd1dea8a2575eff94839140e1dc248aeb87
anothergoldenmonkey <inoue.victorhiroshi@gmail.com>
2019-10-17 22:02:21 +0200

    Add p5.sounds and loop soundtrack

--- src/sketch.js
+++ src/sketch.js
@@ -1,21 +1,42 @@
 /**
+* CONSTANTS AND DEFINITIONS
+*/
+// soundtrack p5.SoundFile container
+let soundtrack;
+
+/**
+* p5.js preload callback.
+*
+* Returns:
+*   undefined;
+*/
+function preload() {
+  // load soundtrack song
+  soundtrack = loadSound('assets/audio/da_beat-nr1.mp3');
+}
+
+/**
  * p5.js setup.
- * 
+ *
  * Returns:
  *  undefined.
  */
-function setup() 
+function setup()
 {
+  // create dummy canvas
+  createCanvas(1, 1);
 
+  // play soundtrack in a loop
+  soundtrack.loop();
 }
 
 /**
  * p5.js drawing callback.
- * 
+ *
  * Returns:
  *  undefined.
  */
-function draw() 
+function draw()
 {
 
 }
