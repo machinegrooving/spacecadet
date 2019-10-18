9a2c2412e8c90f8eb20cd63b2165c53a7b4c3ee3
dmrib <dmrib.cs@gmail.com>
2019-10-17 21:02:08 -0300

    Add sketch configuration parameters

---
+++ config/config.js
@@ -0,0 +1,19 @@
+/**
+* CONSTANTS
+*/
+
+// sketch frame rate
+const FRAME_RATE = 30;
+
+// sketch size values
+const HEIGHT = 400;
+const WIDTH = 1000;
+
+// terrain parameters
+const RESOLUTION = 20;
+const WIDTH_OFFSET = 800;
+const HEIGHT_OFFSET = 200;
+
+// colors
+const BACKGROUND_COLOR = [118, 168, 218];
+const TERRAIN_COLOR = [239, 84, 100, 80];
